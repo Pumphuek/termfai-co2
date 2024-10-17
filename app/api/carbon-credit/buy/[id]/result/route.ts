ebbd7f1688aa9592465afa2332465a411fb0b360
Tumz <pumphuek@outlook.com>
2024-10-17 14:18:04 +0700

    Try to fixed tracking pay result

--- app/api/carbon-credit/buy/[id]/result/route.ts
+++ app/api/carbon-credit/buy/[id]/result/route.ts
@@ -6,6 +6,8 @@ export const dynamic = "force-dynamic";
 export async function GET(request: NextRequest, { params }: { params: { id: string } }) {
   const buyTransaction = await prisma.buyTransaction.findUnique({ where: { id: Number(params.id) } });
 
+  console.log(buyTransaction);
+
   if (!buyTransaction) {
     console.log("Not found buy transaction id.");
     return NextResponse.json({ message: "Not found buy transaction id." }, { status: 404 });
