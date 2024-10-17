af16c6805700a7c0ac808e0f6e6c8fb7a9f92972
Tumz <pumphuek@outlook.com>
2024-10-17 11:59:01 +0700

    Try to log payment result

--- app/api/carbon-credit/buy/[id]/result/route.ts
+++ app/api/carbon-credit/buy/[id]/result/route.ts
@@ -3,7 +3,7 @@ import { NextRequest, NextResponse } from "next/server";
 export const dynamic = "force-dynamic";
 
 export async function POST(request: NextRequest) {
-  const data = await request.json();
+  const data = await request.text();
   console.log(data);
   return NextResponse.json(data);
 }
